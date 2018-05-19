e2f7731079dbf7eeeba4fa6281ade68172832076
Aaron Glick <League@AdministorsiMac.attlocal.net>
2018-05-19 14:23:21 -0700

    I learned binary and hexadecimal today

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,6 +1,15 @@
 package IntroToArrayLists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +18,34 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	JButton button2 = new JButton();
+	ArrayList<String> guests = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		
+	}
+	//oijoiu
+	public void UI() {
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(button);
+		panel.add(button2);
+		button.setText("View Names");
+		button2.setText("Add Names");
+		frame.pack();
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		//JButton buttonPressed = (JButton) e.getSource();
+		
+		if (e.getSource() == button) {
+			JOptionPane.showMessageDialog(null, guests);
+		}
+		
+	}
 }
